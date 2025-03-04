55317808093eddba6d8f2d2831e08812b30d3465
thomasmakak <marsdenthomas@outlook.com>
2025-03-04 10:13:18 +0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,24 @@
+<footer class="container-fluid text-center">
+    <div class="container">
+    <div class="row">
+    <div class="col-md-6">
+      <a class="navbar-brand" href="#"><span class="josh">JOSH</span><span class="orange"> WHITKIN</span></a>
+    
+    </div> 
+
+    <div class="col-md-6">
+        <p>Terms and Conditions</p>
+        
+        </div> 
+
+
+    </div> <!-- row -->
+
+
+    </div> <!-- container -->
+
+
+<?php wp_footer();?>
+</footer>
+</body>
+</html>
